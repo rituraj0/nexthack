cd7a3ab0cec43cbfed735e8164e7dfc3484af907
rituraj0 <rituraj.tc@gmail.com>
2015-05-30 22:06:15 +0530

    port

--- nexthack.py
+++ nexthack.py
@@ -268,7 +268,7 @@ def index():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    #port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0')
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
